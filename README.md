46f5ab923b0742478d3e39f80aa8e378f885a9e9
Speeder <ek.speeder@gmail.com>
2023-12-02 18:29:23 +0100

    The initial version of the automatic patcher script

--- README.md
+++ README.md
@@ -1,2 +1,96 @@
-# revanced-auto-patcher
-Python script to easily patch APKs with ReVanced or RVX
+# ReVanced Automatic Patcher
+This script patches Android applications to change their behaviour. Patches are provided by the [ReVanced](https://github.com/revanced/revanced-patches) or [ReVanced Extended](https://github.com/inotia00/revanced-patches) projects.
+
+## Prerequisites
+The script requires [Python](https://en.wikipedia.org/wiki/Python_(programming_language)) 3 or above to be installed on the computer. Python can be downloaded from [this link](https://www.python.org/downloads), or your operating system's official package manager. The ReVanced tools require Java SDK 11 or above to be installed on the computer. It is recommended to get it from [this link](https://www.azul.com/downloads/?package=jdk#zulu) or your operating system's official package manager.
+
+## Patching
+Both [ReVanced](https://github.com/ReVanced) and [ReVanced Extended](https://github.com/inotia00/) support a limited list of applications and versions. Trying to patch an unsupported application or a supported app's unsupported version will result in errors. The list of supported applications and their versions can be found at the following links:
+
+* [ReVanced](https://revanced.app/patches) (This site has a short update delay)
+* [ReVanced Extended](https://github.com/inotia00/revanced-patches#readme)
+
+The original version of the application must be downloaded before patching can begin. To download a compatible version of the APK file, it is recommended to get it from [APKMirror](https://www.apkmirror.com/), since that site is a fairly safe download source. Find direct links to popular applications below (but choose compatible versions):
+
+* [Facebook](https://www.apkmirror.com/apk/facebook-2/facebook/)
+* [Facebook Messenger](https://www.apkmirror.com/apk/facebook-2/messenger/)
+* [Instagram](https://www.apkmirror.com/apk/instagram/instagram-instagram/)
+* [Lightroom Photo & Video Editor](https://www.apkmirror.com/apk/adobe/lightroom/) (by Adobe)
+* [Recorder](https://www.apkmirror.com/apk/google-inc/google-recorder/) (by Google)
+* [Reddit](https://www.apkmirror.com/apk/redditinc/reddit/)
+* [Spotify](https://apkpure.com/spotify-music-and-podcasts-for-android/com.spotify.music) (warning, not on APKMirror!)
+* [Spotify Lite](https://apkpure.com/spotify-lite/com.spotify.lite) (warning, not on APKMirror!)
+* [Strava](https://www.apkmirror.com/apk/strava-inc/strava-running-and-cycling-gps/)
+* [TikTok](https://www.apkmirror.com/apk/tiktok-pte-ltd/tik-tok/)
+* [Trakt](https://www.apkmirror.com/apk/trakt/trakt/)
+* [Tumblr](https://www.apkmirror.com/apk/tumblr-inc/tumblr/)
+* [Twitch](https://www.apkmirror.com/apk/twitch-interactive-inc/twitch/)
+* [X / Twitter](https://www.apkmirror.com/apk/x-corp/twitter/)
+* [YouTube](https://www.apkmirror.com/apk/google-inc/youtube/)
+* [YouTube Music](https://www.apkmirror.com/apk/google-inc/youtube-music/)
+
+# Default Usage
+By default, the script uses [ReVanced Extended patches](https://github.com/inotia00/revanced-patches) to modify APKs. Only place supported APKs, and only supported versions in this folder, otherwise the patching will fail. Create the following folder structure:
+
+```
+ReVanced
+└── Sources
+    ├── patch.py
+    ├── SomeAppToPatch 10.5.2.apk
+    └── OtherAppToPatch 1.0.0.apk
+```
+
+Then run `python patch.py`. After the script finishes running, you will get the following folder structure:
+
+```
+ReVanced Extended
+├── Sources
+│   ├── tools
+│   │   ├── revanced-cli-*.*.*-all.jar
+│   │   ├── revanced-integrations-*.*.*.apk
+│   │   └── revanced-patches-*.*.*.jar
+│   ├── patch.keystore
+│   ├── patch.py
+│   ├── SomeAppToPatch.json
+│   ├── SomeAppToPatch 10.5.2.apk
+│   ├── OtherAppToPatch.json
+│   └── OtherAppToPatch 1.0.0.apk
+├── RVX SomeAppToPatch 10.5.2.apk
+└── RVX OtherAppToPatch 1.0.0.apk
+```
+
+Where the created directories / files are:
+
+* `tools`: Contains the downloaded ReVanced Extended patches used to patch your APK. Keeping these files can save internet bandwidth when re-patching your APKs.
+* `patch.keystore`: Your unique keys with which the generated APKs were signed. Keep this file to be able to upgrade existing, installed software with newer versions without needing to uninstall the older version.
+* `*.json` files: These files store patch options for the application. Initially these contain default options, but you can edit these files to build customised versions of the patched application.
+* `RVX *.apk`: These are the generated, patched APKs, ready for you to install them.
+
+# Edited Usage
+
+It is possible to relatively easily edit the default behaviour of the patcher script. To edit its default behaviour, open the file in any other text editor application (on Windows you can use notepad). Look for the following text near the top of the file:
+
+```json
+# The following are the default settings. Edit to change the defaults.
+settings = {
+```
+
+The text lines following this contain the configuration of how the script behaves when run without command-line arguments. Each configurable line has a comment (followed by the # character), which explains what that option controls. Change any of these values (to valid ones) to customise the script's behaviour.
+
+For example, to create a directory that applies ReVanced patches, set up a directory as explained in the [Default Usage section](#default-usage), then edit the following line of patch.py from:
+
+```py
+'defaultPatchSource': 'rvx',
+```
+
+to:
+
+```py
+'defaultPatchSource': 'rv',
+```
+
+This makes the script use ReVanced patches instead of ReVanced Extended in that folder. The usage remains the same otherwise.
+
+# Command-line Options
+
+It is possible to thoroughly customise the script's behaviour using command-line arguments, without editing the script. To see the available arguments, run `python patch.py --help`. This will explain the usage of the the command-line interface.
---
+++ patch.py
@@ -0,0 +1,187 @@
+#!/usr/bin/python3
+
+"""
+A python script to easily patch APKs with ReVanced or ReVanced Extended.
+To configure the script's behaviour, edit the default settings below, or run
+"python patch.py --help" to see the command-line argument interface help.
+"""
+
+import os
+import sys
+
+scriptDir = os.path.dirname(os.path.abspath(sys.argv[0]))
+# The following are the default settings. Edit to change the defaults.
+settings = {
+    'srcDir': scriptDir,                                    # Source APKs in the script's directory
+    'outDir': os.path.join(scriptDir, '..'),                # Patched APKs written one directory up
+    'toolsDir': os.path.join(scriptDir, 'tools'),           # The patch tools are downloaded to the 'tools' subdirectory
+    'optionsDir': scriptDir,                                # The patch  configuration options are in the 'options' subdirectory
+    'keystore': os.path.join(scriptDir, 'patch.keystore'),  # The keystore to sign the patched APKs is next to the script
+    'defaultPatchSource': 'rvx',                            # Select whether the default provider should be ReVanced or ReVancedExtended
+    'patchSources': {
+        'rv': {
+            'patches': {
+                'proj': 'revanced/revanced-patches',
+                'ver': 'latest',                            # Use 'latest' or specify the desired release version
+                'type': 'application/java-archive',         # The content type of the file (for download identification)
+            },
+            'integrations': {
+                'proj': 'revanced/revanced-integrations',
+                'ver': 'latest',                            # Use 'latest' or specify the desired release version
+                'type': 'application/vnd.android.package-archive', # The content type of the file (for download identification)
+            },
+            'cli': {
+                'proj': 'revanced/revanced-cli',
+                'ver': 'latest',                            # Use 'latest' or specify the desired release version
+                'type': 'application/java-archive',         # The content type of the file (for download identification)
+            },
+            'prepend': 'RV '                                # Prepend this to the patched output file names
+        },
+        'rvx': {
+            'patches': {
+                'proj': 'inotia00/revanced-patches',
+                'ver': 'latest',                            # Use 'latest' or specify the desired release version
+                'type': 'application/jar',                  # The content type of the file (for download identification)
+            },
+            'integrations': {
+                'proj': 'inotia00/revanced-integrations',
+                'ver': 'latest',                            # Use 'latest' or specify the desired release version
+                'type': 'application/vnd.android.package-archive', # The content type of the file (for download identification)
+            },
+            'cli': {
+                'proj': 'inotia00/revanced-cli',
+                'ver': 'latest',                            # Use 'latest' or specify the desired release version
+                'type': 'application/jar',                  # The content type of the file (for download identification)
+            },
+            'prepend': 'RVX '                               # Prepend this to the patched output file names
+        }
+    }
+}
+
+import argparse
+import glob
+import json
+import re
+import shutil
+import subprocess
+import tempfile
+import urllib.request
+
+class Patcher:
+    tools = ['cli', 'patches', 'integrations']
+
+    def __init__(self, args):
+        patchSourceData = args.patchSrc
+        self.outPrepend = patchSourceData['prepend']
+        self.outDir = args.outDir
+        Patcher.__ensureDirectory(self.outDir)
+        self.toolsDir = args.toolsDir
+        Patcher.__ensureDirectory(self.toolsDir)
+        self.optionsDir = args.optionsDir
+        Patcher.__ensureDirectory(self.optionsDir)
+        self.keystorePath = args.keystore
+        Patcher.__ensureDirectory(os.path.dirname(self.keystorePath))
+        for tool in self.tools:
+            Patcher.__ensureTool(
+                patchSourceData[tool]['proj'],
+                getattr(args, tool + '_version'),
+                patchSourceData[tool]['type'],
+                self.toolsDir)
+        self.toolPaths = {
+            i: glob.glob(os.path.join(self.toolsDir, '*{}*'.format(i)))[0] for i in self.tools
+        }
+
+    def Patch(self, srcPath):
+        srcFile = os.path.basename(srcPath)
+        outPath = os.path.join(self.outDir, self.outPrepend + srcFile)
+        optionsFile = os.path.splitext(Patcher.__normalFileName(srcFile))[0] + '.json'
+        tempDir = os.path.join(tempfile.gettempdir(), 'revanced-resource-cache')
+        print('### Patching {}...'.format(srcFile))
+        try:
+            subprocess.run([
+                'java', '-jar',
+                self.toolPaths['cli'], 'patch',
+                '--patch-bundle=' + self.toolPaths['patches'],
+                '--merge=' + self.toolPaths['integrations'],
+                '--options=' + os.path.join(self.optionsDir, optionsFile),
+                '--keystore=' + self.keystorePath,
+                '--resource-cache=' + tempDir,
+                '--out=' + outPath,
+                srcPath
+            ], stdout=sys.stdout, stderr=sys.stderr, check=True)
+            print('### Finished patching {} successfully!'.format(os.path.abspath(outPath)))
+        except subprocess.CalledProcessError:
+            print('### Failed to patch {}!'.format(srcFile))
+        try:
+            # Purge the temp directory after patching (the built-in purger likes to fail)
+            shutil.rmtree(tempDir)
+        except:
+            pass
+
+    @staticmethod
+    def __ensureDirectory(directory):
+        '''Ensures that the path's directory exists'''
+        try:
+            os.makedirs(directory)
+        except FileExistsError:
+            pass
+
+    @staticmethod
+    def __normalFileName(path):
+        '''Removes version strings from the file name'''
+        regex = r'\b\s*v?\d+(?:\.\d+)*(?:-[^\s]*)?\b'
+        return re.sub(regex, '', path)
+
+    @staticmethod
+    def __ensureTool(project, version, content_type, directory):
+        '''Prepares one ReVanced tool'''
+
+        def clearExistingTools(directory, assetName):
+            '''Deletes older versions of the given tool'''
+            regex = r'^([^\d]*)v?\d+(?:\.\d+(?:\.\d+)?)?[^\d]*(\.[^\.]+)$'
+            assetGlob = re.sub(regex, r'\1*\2', assetName)
+            for file in glob.glob(os.path.join(directory, assetGlob)):
+                os.remove(file)
+
+        if version != 'latest':
+            version = 'tags/v' + version.lstrip('v')
+        url = 'https://api.github.com/repos/{0}/releases/{1}'.format(project, version)
+        releaseData = json.loads(urllib.request.urlopen(url).read())
+        for asset in releaseData['assets']:
+            if (asset['content_type'] == content_type):
+                assetName = asset['name']
+                assetPath = os.path.join(directory, assetName)
+                if (not os.path.exists(assetPath)):
+                    Patcher.__ensureDirectory(directory)
+                    clearExistingTools(directory, assetName)
+                    assetUrl = asset['browser_download_url']
+                    urllib.request.urlretrieve(assetUrl, assetPath)
+
+def main():
+    def raise_(ex): raise ex
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'files', nargs='*',
+        type=lambda x: x if os.path.exists(x) else raise_(argparse.ArgumentTypeError("file not found: " + x)),
+        default=glob.glob(os.path.join(settings['srcDir'], '*.apk')),
+        help='One or more APK file to patch. Patching all APKs in the default source directory if unspecified.')
+    parser.add_argument('--keystore', '-k', default=os.path.abspath(settings['keystore']), help='The path of the keystore file with which to sign the APKs (default: %(default)s)')
+    parser.add_argument('--optionsDir', default=os.path.abspath(settings['optionsDir']), help='The directory to store patch options files in (default: %(default)s)')
+    parser.add_argument('--outDir', '-o', default=os.path.abspath(settings['outDir']), help='The directory to write patched APKs to (default: %(default)s)')
+    parser.add_argument(
+        '--patchSrc', choices=settings['patchSources'], default=settings['defaultPatchSource'],
+        type=lambda x : settings['patchSources'][x], help='The patch source to use. Use "rv" for ReVanced and "rvx" for ReVanced Extended (default: %(default)s).')
+    parser.add_argument('--toolsDir', default=os.path.abspath(settings['toolsDir']), help='The directory to store tools and patches in (default: %(default)s)')
+    for tool in Patcher.tools:
+        parser.add_argument(
+            '--{}-version'.format(tool),
+            type=lambda str : str if re.match(r'^latest|v?\d+(?:\.\d+)*(?:-[^ ]+)?$', str) else raise_(argparse.ArgumentTypeError("invalid version")),
+            default=settings['patchSources'][settings['defaultPatchSource']][tool]['ver'], help='The tool version to use (default: %(default)s)')
+    args = parser.parse_args()
+
+    patcher = Patcher(args)
+    for path in args.files:
+        patcher.Patch(path)
+
+if __name__ == "__main__":
+    main()
